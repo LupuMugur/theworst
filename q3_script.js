49aab62853d502cbfa20683bc96db16f2a877302
alexc <alexandru.cernea@recommerce.com>
2024-04-05 12:34:19 +0300

    feat/q3

---
+++ q3_script.js
@@ -0,0 +1,48 @@
+window.addEventListener("load", () => {
+    document.body.style.cursor = 'none';
+    const questionContainer = document.getElementById("question-container");
+    const questionInput = document.getElementById("question-input");
+    questionInput.tabIndex = -1;
+
+    questionContainer.addEventListener("mouseover", () => {
+        questionContainer.style.backgroundColor = getRandomHexColor();
+    });
+
+    questionInput.addEventListener("mouseover", () => {
+        questionInput.style.height = Math.random() * 150 + "px";
+        questionInput.style.width = Math.random() * 150 + "px";
+    })
+
+    document.addEventListener('mousemove', function(event) {
+        const icon = document.getElementById("icon");
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+    
+        icon.style.left = mouseY + 'px';
+        icon.style.top = mouseX * 0.7 + 'px';
+
+    });
+
+    document.addEventListener('keyup', (event) => {
+        if (event.keyCode === 27) {
+            //go to next question
+
+        }
+    });
+
+});
+
+function getRandomHexColor() {
+    
+    var red = Math.floor(Math.random() * 256); 
+    var green = Math.floor(Math.random() * 256);
+    var blue = Math.floor(Math.random() * 256);
+
+    var redHex = red.toString(16).padStart(2, '0'); 
+    var greenHex = green.toString(16).padStart(2, '0');
+    var blueHex = blue.toString(16).padStart(2, '0');
+
+    var hexColor = '#' + redHex + greenHex + blueHex;
+
+    return hexColor;
+}
